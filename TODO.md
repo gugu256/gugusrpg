aa3e7ca0ace2e09eaedc354f9b1863f3ae96ccc1
gugu256 <gugu256@mail.com>
2024-07-16 17:57:21 +0200

    added the day function, added new food mechanics as well as death (this is now officially an eating game!)

--- TODO.md
+++ TODO.md
@@ -1,6 +1,12 @@
 # TODO
 
-- [ ] set_passage()
-- [ ] show_health()
-- [ ] show_food()
+- [ ] decide whether to clear the terminal all the time or not
+- [ ] night() - sunset, eat .5 food (dinner), die if health too low, fall asleep
+- [ ] small random events during day() and night(): get attacked by an animal at night, wake up with your stuff stolen 
+- [x] check_health() die if too low
+- [x] skipameal()
+- [x] day() - sunrise, wake up, eat .5 food (breakfast), die if health too low
+- [?] set_passage() - i don't even remember why this was important
+- [x] show_health()
+- [x] show_food()
 - [x] load()
--- game.py
+++ game.py
@@ -4,13 +4,15 @@ import os, time, random
 
 ### LIB
 ## VARIABLES
-health = 100
-food = 0 # this variable = the number of days you can last with the amount of food you have
+health = 1
+food = 1 # this variable = the number of days you can last with the amount of food you have
 location = "Genève"
 coins = 0
 languages = []
 inv = []
-day = 0
+day_n = 0
+b = "b"
+d = "d"
 
 ## CLASSES
 
@@ -35,7 +37,7 @@ def load(t, l=10, task=""):
         clear()
 
 def pe():
-  input("Press Enter to continue./")
+  input("\nPress Enter to continue/")
 
 def q():
     choose("Are you sure you want to quit?", [quit, pe], ["Yes", "No"])
@@ -56,24 +58,46 @@ def show_loc(now = False):
     print(f"You are currently in {location}.")
     pe()
 
-def show_food():
-    if food == 0:
-        print("You have no food!")
-    elif food == 1:
-        print("You have enough food to last you 1 day")
+def show_food(now=False, pressenter=True):
+    if now:
+        if food == 0:
+            print("You now have no food!")
+        elif food == 1:
+            print("You now have enough food to last you 1 day")
+        else:
+            print(f"You now have enough food to last you {food} days")
     else:
-        print(f"You have enough food to last you {food} days")
-    pe()
+        if food == 0:
+            print("You have no food!")
+        elif food == 1:
+            print("You have enough food to last you 1 day")
+        else:
+            print(f"You have enough food to last you {food} days")
+    if pressenter: pe()
 
-def choose(msg, passages, passages_name):
-    clear()
-    print(msg)
+def show_health(now=False, pressenter=True):
+    if now == True: print(f"Your health level is now {health}")
+    else: print(f"Your health level is currently {health}")
+    if pressenter: pe()
+
+def show_day(pressenter=True):
+    print(f"Day {day_n}")
+    if pressenter: pe()
+
+# other library functions
+
+def choose(msg, passages, passages_name, clearscreen=True):
+    if clearscreen: 
+        clear()
+
+    print(msg + "\n")
     i = 1
     for passage in passages_name:
         print(f"{i} : {passage}")
         i += 1
     c = input("> ")
     if is_int(c) and int(c) >= 1 and int(c) < i:
+        print()
         passages[int(c)-1]()
     elif c == "inv":
         show_inv()
@@ -89,10 +113,56 @@ def choose(msg, passages, passages_name):
     else:
         choose(msg, passages, passages_name)
 
+# game functions (not passages)
+def die(cause="none"):
+    if cause == "none":
+        print("You died!")
+    elif cause == "hunger":
+        print("You starved to death!")
+    pe()
+
+def checkhealth(cause="none"):
+    if health > 0:
+        pass
+    else:
+        die(cause)
+
+def eat(t=b):
+    global food
+    if food >= 0.5:
+        food -= 0.5
+        if t == "b":
+            print("You eat breakfast, your health stays the same")
+            show_food(True, False)
+            show_health()
+        elif t == "d":
+            print("You eat dinner, your health stays the same")
+            show_food(True, False)
+            show_health()
+    else:
+        if t == "b": print("You don't have any food! You cannot eat breakfast")
+        elif t == "d": print("You don't have any food! You cannot eat dinner")
+        skipameal()    
+
+def skipameal():
+    global health
+    print("You skip a meal, but your health level decreases by 1...")
+    health -= 1
+    show_health(True, False)
+    checkhealth("hunger")
+
+def day():
+    clear()
+    global day_n
+    day_n += 1
+    show_day(False)
+    print("The sun rises, and you wake up...")
+    # RANDOM EVENT HERE
+    choose("Do you want to eat breakfast?", [eat, skipameal], ["Let's eat breakfast!","I can skip a meal..."], False)
 
 # Maybe we should create character aptitudes and specialties like intelligence, strength
 
-# You can start the game as a venician merchant, a portuguese merchant, a persian merchant, a chinese merchant
+# You can start the game as a venician merchant, a portugese merchant, a persian merchant, a chinese merchant
 
 ### STORY/NOTES
 """Your father recently passed away, and you are his only child left. You have to keep the family business on, and trade spices, silk or ores on the Silk road
@@ -104,6 +174,3 @@ Your goal, is to become profitable and make your family's name remembered. But m
 # Some passages end days (for example getting somewhere or trekking), some don't (for example meeting people doesn't make the day end), some end your life and you have to start over!
 
 ## PASSAGES DEFINITIONS
-
-while True:
-    q()
